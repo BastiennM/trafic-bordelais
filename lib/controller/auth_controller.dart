8d2782671aec45c5d7371a7a184626a91dc159b3
Metais Bastien <bastienmetais79@gmail.com>
2023-01-27 20:32:04 +0100

    :sparkles: (auth) : Add auth controller

---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,239 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:getx_templates/ui/widgets/custom_progress_indicator.dart';
+
+class AuthController extends GetxController {
+  static AuthController to = Get.find();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  Rxn<User> firebaseUser = Rxn<User>();
+  Rxn<UserModel> firestoreUser = Rxn<UserModel>();
+  final RxBool admin = false.obs;
+  CustomProgressIndicator customProgressIndicator = const CustomProgressIndicator();
+  @override
+  void onReady() async {
+    //run every time auth state changes
+    ever(firebaseUser, handleAuthChanged);
+
+    firebaseUser.bindStream(user);
+
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.onClose();
+  }
+
+  handleAuthChanged(_firebaseUser) async {
+    //get user data from firestore
+    if (_firebaseUser?.uid != null) {
+      firestoreUser.bindStream(streamFirestoreUser());
+      await isAdmin();
+    }
+
+    if (_firebaseUser == null) {
+      print('Send to signin');
+      //TODO : Add sign in screen
+      //Get.offAll(SignInUI());
+    } else {
+      print("send to register");
+      //TODO: Add register screen
+      //Get.offAll(HomeUI());
+    }
+  }
+
+  // Firebase user one-time fetch
+  Future<User> get getUser async => _auth.currentUser!;
+
+  // Firebase user a realtime stream
+  Stream<User?> get user => _auth.authStateChanges();
+
+  //Streams the firestore user from the firestore collection
+  Stream<UserModel> streamFirestoreUser() {
+    print('streamFirestoreUser()');
+
+    return _db
+        .doc('/users/${firebaseUser.value!.uid}')
+        .snapshots()
+        .map((snapshot) => UserModel.fromMap(snapshot.data()!));
+  }
+
+  //get the firestore user from the firestore collection
+  Future<UserModel> getFirestoreUser() {
+    return _db.doc('/users/${firebaseUser.value!.uid}').get().then(
+            (documentSnapshot) => UserModel.fromMap(documentSnapshot.data()!));
+  }
+
+  //Method to handle user sign in using email and password
+  signInWithEmailAndPassword(BuildContext context) async {
+    customProgressIndicator.show();
+    try {
+      await _auth.signInWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim());
+      emailController.clear();
+      passwordController.clear();
+      customProgressIndicator.close();
+    } catch (error) {
+      customProgressIndicator.close();
+      Get.snackbar('auth.signInErrorTitle'.tr, 'auth.signInError'.tr,
+          snackPosition: SnackPosition.BOTTOM,
+          duration: Duration(seconds: 7),
+          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
+          colorText: Get.theme.snackBarTheme.actionTextColor);
+    }
+  }
+
+  // User registration using email and password
+  registerWithEmailAndPassword(BuildContext context) async {
+    customProgressIndicator.show();
+    try {
+      await _auth
+          .createUserWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text)
+          .then((result) async {
+        print('uID: ${result.user!.uid}');
+        print('email: ${result.user!.email}');
+
+        //create the new user object
+        UserModel newUser = UserModel(
+            uid: result.user!.uid,
+            email: result.user!.email!,
+            name: nameController.text
+        );
+        //create the user in firestore
+        _createUserFirestore(newUser, result.user!);
+        emailController.clear();
+        passwordController.clear();
+        customProgressIndicator.close();
+      });
+    } on FirebaseAuthException catch (error) {
+      customProgressIndicator.close();
+      Get.snackbar('auth.signUpErrorTitle'.tr, error.message!,
+          snackPosition: SnackPosition.BOTTOM,
+          duration: Duration(seconds: 10),
+          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
+          colorText: Get.theme.snackBarTheme.actionTextColor);
+    }
+  }
+
+  //handles updating the user when updating profile
+  Future<void> updateUser(BuildContext context, UserModel user, String oldEmail,
+      String password) async {
+    String authUpdateUserNoticeTitle = 'auth.updateUserSuccessNoticeTitle'.tr;
+    String authUpdateUserNotice = 'auth.updateUserSuccessNotice'.tr;
+    try {
+      customProgressIndicator.show();
+      try {
+        await _auth
+            .signInWithEmailAndPassword(email: oldEmail, password: password)
+            .then((_firebaseUser) async {
+          await _firebaseUser.user!
+              .updateEmail(user.email)
+              .then((value) => _updateUserFirestore(user, _firebaseUser.user!));
+        });
+      } catch (err) {
+        print('Caught error: $err');
+        //not yet working, see this issue https://github.com/delay/flutter_starter/issues/21
+        if (err.toString() ==
+            "[firebase_auth/email-already-in-use] The email address is already in use by another account.") {
+          authUpdateUserNoticeTitle = 'auth.updateUserEmailInUse'.tr;
+          authUpdateUserNotice = 'auth.updateUserEmailInUse'.tr;
+        } else {
+          authUpdateUserNoticeTitle = 'auth.wrongPasswordNotice'.tr;
+          authUpdateUserNotice = 'auth.wrongPasswordNotice'.tr;
+        }
+      }
+      customProgressIndicator.close();
+      Get.snackbar(authUpdateUserNoticeTitle, authUpdateUserNotice,
+          snackPosition: SnackPosition.BOTTOM,
+          duration: const Duration(seconds: 5),
+          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
+          colorText: Get.theme.snackBarTheme.actionTextColor);
+    } on PlatformException catch (error) {
+      customProgressIndicator.close();
+      print(error.code);
+      String authError;
+      switch (error.code) {
+        case 'ERROR_WRONG_PASSWORD':
+          authError = 'auth.wrongPasswordNotice'.tr;
+          break;
+        default:
+          authError = 'auth.unknownError'.tr;
+          break;
+      }
+      Get.snackbar('auth.wrongPasswordNoticeTitle'.tr, authError,
+          snackPosition: SnackPosition.BOTTOM,
+          duration: const Duration(seconds: 10),
+          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
+          colorText: Get.theme.snackBarTheme.actionTextColor);
+    }
+  }
+
+  //updates the firestore user in users collection
+  void _updateUserFirestore(UserModel user, User _firebaseUser) {
+    _db.doc('/users/${_firebaseUser.uid}').update(user.toJson());
+    update();
+  }
+
+  //create the firestore user in users collection
+  void _createUserFirestore(UserModel user, User _firebaseUser) {
+    _db.doc('/users/${_firebaseUser.uid}').set(user.toJson());
+    update();
+  }
+
+  //password reset email
+  Future<void> sendPasswordResetEmail(BuildContext context) async {
+    customProgressIndicator.show();
+    try {
+      await _auth.sendPasswordResetEmail(email: emailController.text);
+      customProgressIndicator.close();
+      Get.snackbar(
+          'auth.resetPasswordNoticeTitle'.tr, 'auth.resetPasswordNotice'.tr,
+          snackPosition: SnackPosition.BOTTOM,
+          duration: const Duration(seconds: 5),
+          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
+          colorText: Get.theme.snackBarTheme.actionTextColor);
+    } on FirebaseAuthException catch (error) {
+      customProgressIndicator.close();
+      Get.snackbar('auth.resetPasswordFailed'.tr, error.message!,
+          snackPosition: SnackPosition.BOTTOM,
+          duration: const Duration(seconds: 10),
+          backgroundColor: Get.theme.snackBarTheme.backgroundColor,
+          colorText: Get.theme.snackBarTheme.actionTextColor);
+    }
+  }
+
+  //check if user is an admin user
+  isAdmin() async {
+    await getUser.then((user) async {
+      DocumentSnapshot adminRef =
+      await _db.collection('admin').doc(user.uid).get();
+      if (adminRef.exists) {
+        admin.value = true;
+      } else {
+        admin.value = false;
+      }
+      update();
+    });
+  }
+
+  // Sign out
+  Future<void> signOut() {
+    nameController.clear();
+    emailController.clear();
+    passwordController.clear();
+    return _auth.signOut();
+  }
+}
